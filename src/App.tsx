dbf6a8746fc2777db608979bfaf70567c3132479
Ryan Christiani <ryan.christiani@shopify.com>
2019-02-05 12:31:31 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>TypeScript</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
